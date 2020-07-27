0916becca33da26f351a5c5a001e2048be5f13d9
Zein <hualzein@gmail.com>
2020-07-27 08:25:58 -0400

    Added realtime re-caching of reminders+date format

--- Main/BankBot.py
+++ Main/BankBot.py
@@ -1,10 +1,13 @@
 import os, os.path
 import discord
+from discord import NotFound
 import sys
 import random
 import re
 import errno
+from datetime import datetime
 from dotenv import load_dotenv
+from file_read_backwards import FileReadBackwards
 
 load_dotenv()                       #loads .env file in the same directory -- use for configs and any changes -- dont edit settings in this file, use the env file
 TOKEN = os.getenv('DISCORD_TOKEN')  #loads PRIVATE discord token that we set as an env variable in the .env file -- this is done so you don't post private token to VCs software i.e github
@@ -55,13 +58,30 @@ class customClient(discord.Client):
             except OSError as e:
                 if e.errno != errno.EEXIST:                         #Only raise exception if the error is NOT that the directory already exists
                     raise
-            with open(curGUILD.name.replace(" ", "") + "/reminderCache.txt", encoding="utf8", mode="a") as outp: #keep reminder caches seperated by folders named after each server - special characters probably not handled here..
+            afterVal = None
+            try:
+                with open(curGUILD.name.replace(" ", "") + "/reminderCache.txt", "r") as dateCheck: #pull the most recent reminder (always the first reminder in file)
+                    first_line = dateCheck.readline()
+                    print(first_line)
+                    afterValTemp = re.findall('\[\!\[(.*?)\[\!\[', first_line)                      #date format surrounded by [![ for a guarateed disambiguation
+                    if(len(afterValTemp)>0):                                                        #if we got a valid date, should happen every time but who knows
+                        afterVal = datetime.fromisoformat(afterValTemp[0])
+                        print(f'LAST MESSAGE DATE RECORDED: {afterValTemp[0]}')
+                    else:
+                        afterVal = None
+                if afterVal != None:                                                            # we now want our current file shoved elsewhere
+                    os.rename(curGUILD.name.replace(" ", "") + "/reminderCache.txt",            # so we can start writing our new reminders and add the old stuff under
+                      curGUILD.name.replace(" ", "") + "/reminderCache2.txt")                   #to maintain our first line = most recent reminder assumption
+            except OSError as e:
+                if e.errno != errno.ENOENT:
+                    raise
+            with open(curGUILD.name.replace(" ", "") + "/reminderCache.txt", encoding="utf8", mode="w") as outp: #keep reminder caches seperated by folders named after each server - special characters probably not handled here..
                 for curCHANNEL in curGUILD.channels:
                     if curCHANNEL.type is discord.ChannelType.text: #only loop through text channels
                         print(f'{curCHANNEL.name} is of type {curCHANNEL.type}\n')
-                        async for curMESSAGE in curCHANNEL.history(limit=None): #for every message in this channel's history
+                        async for curMESSAGE in curCHANNEL.history(limit=None, after=afterVal): #for every message in this channel's history
                             if curMESSAGE.content.lower().find("reminder") >= 0 and curMESSAGE.content.lower().find("!") != 0 and curMESSAGE.author != CLIENT.user:
-                                curVal = f'{curMESSAGE.author.mention} {curMESSAGE.content} ' #Formats the beginning of the string to mention the user and post the reminder itself
+                                curVal = f'{curMESSAGE.author.mention} {curMESSAGE.content} [![{curMESSAGE.created_at}[![' #Formats the beginning of the string to mention the user and post the reminder itself
                                 for attachment in curMESSAGE.attachments:                     #check for files attached
                                     random.seed()                                             #random seed so duplicate file names almost never happen
                                     filName = f'{curGUILD.name.replace(" ","")}/attachments/{random.randint(1,1000)}{attachment.filename}' #append random value to file name
@@ -72,7 +92,28 @@ class customClient(discord.Client):
                                 print(curVal)
                     else:
                         print(f'{curCHANNEL.name} is of type {curCHANNEL.type}\n')
-            outp.close()
+            if afterVal != None:
+                os.rename(curGUILD.name.replace(" ", "")+"/reminderCache.txt",
+                          curGUILD.name.replace(" ", "")+"/reminderCache3.txt")
+                with open(curGUILD.name.replace(" ", "")+"/reminderCache.txt", mode="a") as mostRecentReminders:
+                    with FileReadBackwards(curGUILD.name.replace(" ", "")+"/reminderCache3.txt", encoding="utf-8") as reversedFile:
+                        for reversedLines in reversedFile:
+                            mostRecentReminders.write(reversedLines+"\n")
+                with open(curGUILD.name.replace(" ", "")+"/reminderCache.txt", mode="a") as combineFiles:
+                    with open(curGUILD.name.replace(" ", "")+"/reminderCache2.txt") as sideFile:
+                        for sideLine in sideFile:
+                            combineFiles.write(sideLine)
+                os.remove(curGUILD.name.replace(" ", "")+"/reminderCache2.txt")
+                os.remove(curGUILD.name.replace(" ", "")+"/reminderCache3.txt")
+            with open(curGUILD.name.replace(" ", "")+"/reminderCache.txt", mode="r+") as afterChecker: #remove new line at the end
+                afterChecker.seek(0, os.SEEK_END)                     # Move the pointer (similar to a cursor in a text editor) to the end of the file
+                pos = afterChecker.tell() - 1                         # This code means the following code skips the very last character in the file -
+                while pos > 0 and afterChecker.read(1) != "\n":       # Read each character in the file one at a time from the penultimate
+                    pos -= 1
+                    afterChecker.seek(pos, os.SEEK_SET)
+                if pos > 0:                                           # So long as we're not at the start of the file, delete all the characters ahead
+                    afterChecker.seek(pos, os.SEEK_SET)
+                    afterChecker.truncate()                           #All this just to ensure we dont have a new line at the end of our file..
 
         if message.content == "!reminder":
             with open(message.guild.name.replace(" ", "") + "/reminderCache.txt", encoding="utf8") as reminp:        #always pass encoding in when opening a file, or else emojis and special chars fail and shit your text up
@@ -83,9 +124,8 @@ class customClient(discord.Client):
                 fileObjectList = []
                 for names in attachmentsList:
                     fileObjectList.append(discord.File(names))      #must convert file-like objects to discord files before appending them to the file list
-                responseFormatted = response.partition("|")         #remove the attachment tag before posting the reminder
+                responseFormatted = response.partition("[![")         #remove the attachment tag before posting the reminder
             await message.channel.send(content=responseFormatted[0], files=fileObjectList)  #syntax for sending a message with both text and files
-            reminp.close()
 
 CLIENT = customClient()
 CLIENT.run(TOKEN)                   #turn bot on -- buy the bot dinner prior to this step
