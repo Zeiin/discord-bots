2e24b3cedf71cffd70bb3d965e227b69e5d71417
Zein <hualzein@gmail.com>
2020-08-20 03:59:06 -0400

    added files

--- Main/Bakamitai.bat
+++
@@ -1,1 +0,0 @@
-python MLDeepFake/first-order-model/demo.py --config config/vox-256.yaml --driving_video resources/Base.mp4 --source_image resources/fez.png --checkpoint resources/vox-cpk.pth.tar --relative --adapt_scale
